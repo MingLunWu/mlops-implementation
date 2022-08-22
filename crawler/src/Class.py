fe7a3bb4367e1bd983967c01b7a435658e17ebe1
MingLunWu <allen6997535@gmail.com>
2022-08-22 15:03:41 +0000

    [modify] try to design some object

---
+++ crawler/src/Class.py
@@ -0,0 +1,8 @@
+class Toc():
+    def __init__(self, base_url: str, idx:int) -> None:
+        pass
+        self.articles = []
+    
+class Article():
+    def __init__(self) -> None:
+        pass
