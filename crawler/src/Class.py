835bac9f606bdc18b6b92315f2bd771430d86e65
MingLunWu <allen6997535@gmail.com>
2022-08-23 15:00:28 +0000

    [modify] add basic Article structure

--- crawler/src/Class.py
+++ crawler/src/Class.py
@@ -1,8 +1,24 @@
+from bs4.element import Tag
+import logging
 class Toc():
     def __init__(self, base_url: str, idx:int) -> None:
         pass
         self.articles = []
     
 class Article():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, bs4_tag: Tag) -> None:
+        """_summary_
+
+        Args:
+            bs4_tag (Tag): bs4 tag (soup.find_all("div", "r-ent"))
+        """
+        try:
+            self.author = bs4_tag.find("div","author").text
+            self.title =  bs4_tag.find("div", "title").a.text
+            self.url =  bs4_tag.find("div", "title").a['href']
+            self.date = bs4_tag.find("div","date").text
+        except AttributeError as e:
+            logging.error(e)
+            logging.error(bs4_tag.prettify())
+
+    
