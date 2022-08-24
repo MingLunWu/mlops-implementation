d50367b1cb75d2b64852a434aa24cec344531882
MingLunWu <allen6997535@gmail.com>
2022-08-24 15:27:26 +0000

    [modify] create comment obj

--- crawler/src/Article.py
+++ crawler/src/Article.py
@@ -1,6 +1,7 @@
+from crawler.src.utils import create_soup, PTT_PREFIX
 class Article:
     def __init__(self, title: str, author: str, url: str, date: str) -> None:
-        """_summary_
+        """Constructor
 
         Args:
             title (str): The title of the article
@@ -11,6 +12,20 @@ class Article:
         self.author = author
         self.title =  title
         self.url =  url
+        self.date = date
+        self.content = None
+        self.comments = []
+        
+        self._soup = create_soup(PTT_PREFIX + self.url)
+
+    def extract_comments(self) -> None:
+        pass
+
+class Comment:
+    def __init__(self, author: str, status: str, content: str, date: str) -> None:
+        self.author = author
+        self.status = status
+        self.content = content
         self.date = date
 
     
