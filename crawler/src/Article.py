5f24f7e337ed161f384273e9ab160b38a6594aa5
MingLunWu <allen6997535@gmail.com>
2022-08-25 12:30:13 +0000

    [modify] add comment(push)

--- crawler/src/Article.py
+++ crawler/src/Article.py
@@ -1,3 +1,4 @@
+import logging
 from crawler.src.utils import create_soup, PTT_PREFIX
 class Article:
     def __init__(self, title: str, author: str, url: str, date: str) -> None:
@@ -19,7 +20,32 @@ class Article:
         self._soup = create_soup(PTT_PREFIX + self.url)
 
     def extract_comments(self) -> None:
-        pass
+        self._extract_push_comments()
+        print("test")
+
+    def _extract_push_comments(self) -> None:
+        successed, failed = 0, 0
+        for tag in self._soup.find_all("div", "push"):
+            try:
+                author = tag.find("span", "push-userid").text
+                content = tag.find("span", "push-content").text[2:] # The first two characters are ": "
+                date = tag.find("span", "push-ipdatetime").text
+                status = "push"
+                comment_obj = Comment(
+                    author=author,
+                    status=status,
+                    content=content,
+                    date=date
+                )
+                self.comments.append(comment_obj)
+                tag.decompose()
+                successed += 1
+            except Exception as e:
+                logging.error(e)
+                tag.decompose()
+                failed += 1
+
+        logging.info(f"{successed} comments (push) has been successfully extracted, {failed} has been failed.")
 
 class Comment:
     def __init__(self, author: str, status: str, content: str, date: str) -> None:
